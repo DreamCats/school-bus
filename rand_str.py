b3dd7cdefe176a6da7138ac87a276ee97043ca3a
DreamCats <maifeng_cat@outlook.com>
2019-01-22 15:53:48 +0800

    随机生成一定长度的字符串，测试完毕

--- rand_str.py
+++ rand_str.py
@@ -1,3 +1,29 @@
 # __author__: Mai feng
 # __file_name__: rand_str.py
 # __time__: 2019:01:22:15:45
+import random
+class RandStr(object):
+    def __init__(self, length):
+        '''初始化
+        :param length: 字符串长度
+        '''
+        self.str = 'zxcvbnmlkjhgfdsaqwertyuiopPOIUYTREWQASDFGHJKLMNBVCXZ1234567890'
+        self.length = length
+
+    def get_rand_str(self):
+        '''返回一定长度的随机字符串
+        :return: 和功能说明一样
+        '''
+        random.seed()
+        number = []
+        for i in range(self.length):
+            number.append(random.choice(self.str))
+        res = ''.join(number)
+        return res
+
+
+if __name__ == "__main__":
+    rand_str = RandStr(length=5)
+    res = rand_str.get_rand_str()
+    print(res)
+
