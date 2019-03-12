084e4f41c64b0d69cfc1e168626761c5a7fca21b
DreamCats <maifeng_cat@outlook.com>
2019-03-12 11:19:58 +0800

    定义person模型

--- mysql/pw.py
+++ mysql/pw.py
@@ -3,3 +3,28 @@
 # __time__: 2019:03:12:10:52
 
 # 利用peewee操作mysql
+from peewee import *
+
+
+database = MySQLDatabase(
+    'pw',
+    user='root',
+    password='123',
+    host='localhost',
+    port=3306
+)
+
+# 定义Person模型
+class Person(Model):
+    name = CharField()
+    birthday = DateField()
+    is_relative = BooleanField()
+
+    class Meta:
+        database = database
+
+# 创建Person
+# Person.create_table()
+
+# 添加一条数据
+
