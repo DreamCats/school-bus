9b4862f0162c8c142e24574968c6a3592c6a2688
DreamCats <maifeng_cat@outlook.com>
2020-02-25 20:50:31 +0800

    生成sb_user_t的数据模型

---
+++ school-bus/guns-user/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/SbUserTMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest.common.persistence.dao;
+
+import com.stylefeng.guns.rest.common.persistence.model.SbUserT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户表 Mapper 接口
+ * </p>
+ *
+ * @author Maifeng
+ * @since 2020-02-25
+ */
+public interface SbUserTMapper extends BaseMapper<SbUserT> {
+
+}
