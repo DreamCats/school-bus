83cd56b942313d162720edf636b3783ebbfbb40c
DreamCats <maifeng_cat@outlook.com>
2020-03-01 16:22:29 +0800

    create IBusService

---
+++ school-bus/guns-api/src/main/java/com/stylefeng/guns/rest/bus/IBusService.java
@@ -0,0 +1,10 @@
+/**
+ * @program school-bus
+ * @description: IBusService:班车服务
+ * @author: mf
+ * @create: 2020/03/01 16:21
+ */
+package com.stylefeng.guns.rest.bus;
+
+public interface IBusService {
+}
