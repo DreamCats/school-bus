3707586a572f570e5691f05b2858331d0b34e0d6
DreamCats <maifeng_cat@outlook.com>
2020-03-18 22:50:05 +0800

    格式化常量值

--- school-bus/guns-api/src/main/java/com/stylefeng/guns/rest/common/constants/RedisConstants.java
+++ school-bus/guns-api/src/main/java/com/stylefeng/guns/rest/common/constants/RedisConstants.java
@@ -10,14 +10,14 @@ package com.stylefeng.guns.rest.common.constants;
 
 public enum RedisConstants {
 
-    TOKEN_EXPIRE                                 (3600, "token过期时间"), // 1 小时
-    USER_INFO_EXPIRE                             (3600, "用户信息过期时间", "getUserById "), //1小时
-    COUNT_DETAIL_EXPIRE                             (600, "场次详情过期时间", "getCountDetailById "),// 10分钟
-    COUNTS_EXPIRE                                (600, "场次列表", "getCount "),// 10分钟
-    NO_TAKE_OREDERS_EXPIRE                                (600, "未乘坐订单列表", "getNoTakeOrdersById "),// 10分钟
-    NO_PAY_ORDERS_EXPIRE                                (600, "未支付订单列表", "getNoPayOrdersById "),// 10分钟
-    EVALUATE_ORDERS_EXPIRE                                (600, "评价订单列表", "getEvaluateOrdersById "),// 10分钟
-    SELECT_ORDER_EXPIRE                                (600, "订单详情", "selectOrderById ");// 10分钟
+    TOKEN_EXPIRE(3600, "token过期时间"), // 1 小时
+    USER_INFO_EXPIRE(3600, "用户信息过期时间", "getUserById "), //1小时
+    COUNT_DETAIL_EXPIRE(600, "场次详情过期时间", "getCountDetailById "),// 10分钟
+    COUNTS_EXPIRE(600, "场次列表", "getCount "),// 10分钟
+    NO_TAKE_OREDERS_EXPIRE(600, "未乘坐订单列表", "getNoTakeOrdersById "),// 10分钟
+    NO_PAY_ORDERS_EXPIRE(600, "未支付订单列表", "getNoPayOrdersById "),// 10分钟
+    EVALUATE_ORDERS_EXPIRE(600, "评价订单列表", "getEvaluateOrdersById "),// 10分钟
+    SELECT_ORDER_EXPIRE(600, "订单详情", "selectOrderById ");// 10分钟
 
     private Integer time;
     private String message;
--- school-bus/guns-api/src/main/java/com/stylefeng/guns/rest/common/constants/RetCodeConstants.java
+++ school-bus/guns-api/src/main/java/com/stylefeng/guns/rest/common/constants/RetCodeConstants.java
@@ -9,35 +9,34 @@ package com.stylefeng.guns.rest.common.constants;
 
 public enum RetCodeConstants {
     // 系统公用
-    SUCCESS                             ("000000", "成功"),
+    SUCCESS("000000", "成功"),
+    USERORPASSWORD_ERRROR("003001", "用户名或密码不正确"),
+    TOKEN_VALID_FAILED("003002", "token校验失败"),
+    USERNAME_ALREADY_EXISTS("003003", "用户名已存在"),
+    USER_REGISTER_FAILED("003004", "注册失败，请联系管理员"),
+    KAPTCHA_CODE_ERROR("003005", "验证码不正确"),
+    USER_ISVERFIED_ERROR("003006", "用户名尚未激活"),
+    USER_REGISTER_VERIFY_FAILED("003007", "用户注册失败插入验证数据失败"),
+    USER_INFOR_INVALID("003004", "用户信息不合法"),
 
-    USERORPASSWORD_ERRROR               ("003001","用户名或密码不正确"),
-    TOKEN_VALID_FAILED                  ("003002","token校验失败"),
-    USERNAME_ALREADY_EXISTS             ("003003","用户名已存在"),
-    USER_REGISTER_FAILED                ("003004","注册失败，请联系管理员"),
-    KAPTCHA_CODE_ERROR                  ("003005","验证码不正确"),
-    USER_ISVERFIED_ERROR                ("003006","用户名尚未激活"),
-    USER_REGISTER_VERIFY_FAILED         ("003007","用户注册失败插入验证数据失败"),
-    USER_INFOR_INVALID                  ("003004","用户信息不合法"),
+    REQUEST_FORMAT_ILLEGAL("003060", "请求数据格式非法"),
+    REQUEST_IP_ILLEGAL("003061", "请求IP非法"),
+    REQUEST_CHECK_FAILURE("003062", "请求数据校验失败"),
+    DATA_NOT_EXIST("003070", "数据不存在"),
+    DATA_REPEATED("003071", "数据重复"),
+    REQUEST_DATA_NOT_EXIST("003072", "传入对象不能为空"),
+    REQUEST_DATA_ERROR("003074", "必要的参数不正确"),
+    REQUISITE_PARAMETER_NOT_EXIST("003073", "必要的参数不能为空"),
+    PERMISSION_DENIED("003091", "权限不足"),
+    DB_EXCEPTION("003097", "数据库异常"),
+    SYSTEM_TIMEOUT("003098", "系统超时"),
+    SYSTEM_ERROR("003099", "系统错误"),
 
-    REQUEST_FORMAT_ILLEGAL              ("003060", "请求数据格式非法"),
-    REQUEST_IP_ILLEGAL                  ("003061", "请求IP非法"),
-    REQUEST_CHECK_FAILURE               ("003062", "请求数据校验失败"),
-    DATA_NOT_EXIST                      ("003070", "数据不存在"),
-    DATA_REPEATED                       ("003071", "数据重复"),
-    REQUEST_DATA_NOT_EXIST              ("003072", "传入对象不能为空"),
-    REQUEST_DATA_ERROR                  ("003074", "必要的参数不正确"),
-    REQUISITE_PARAMETER_NOT_EXIST       ("003073", "必要的参数不能为空"),
-    PERMISSION_DENIED                   ("003091", "权限不足"),
-    DB_EXCEPTION                        ("003097", "数据库异常"),
-    SYSTEM_TIMEOUT                      ("003098", "系统超时"),
-    SYSTEM_ERROR                        ("003099", "系统错误"),
+    USERVERIFY_INFOR_INVALID("003200", "用户注册验证验证信息不合法"),
 
-    USERVERIFY_INFOR_INVALID            ("003200", "用户注册验证验证信息不合法"),
-
-    SELECTED_SEATS                      ("004000", "座位已被选择，请重新选择座位"),
-    PAY_PASSWORD_ERROR                  ("004001", "支付密码错误"),
-    MONEY_ERROR                         ("004002", "用户余额不足，请充值");
+    SELECTED_SEATS("004000", "座位已被选择，请重新选择座位"),
+    PAY_PASSWORD_ERROR("004001", "支付密码错误"),
+    MONEY_ERROR("004002", "用户余额不足，请充值");
 
     private String code;
     private String message;
