d6d2c47f095c5468397cb2491927ef7adf6b20be
DreamCats <maifeng_cat@outlook.com>
2020-04-04 11:22:29 +0800

    对场次列表进行压力测试

--- school-bus/guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/bus/BusController.java
+++ school-bus/guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/bus/BusController.java
@@ -8,12 +8,14 @@
 package com.stylefeng.guns.rest.modular.bus;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.stylefeng.guns.core.constants.SbCode;
 import com.stylefeng.guns.rest.bus.IBusService;
 import com.stylefeng.guns.rest.bus.dto.*;
 import com.stylefeng.guns.rest.common.RedisUtils;
 import com.stylefeng.guns.rest.common.ResponseData;
 import com.stylefeng.guns.rest.common.ResponseUtil;
 import com.stylefeng.guns.core.constants.RedisConstants;
+import com.stylefeng.guns.rest.exception.CommonResponse;
 import com.stylefeng.guns.rest.modular.form.CountPageInfo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
@@ -43,22 +45,32 @@ public class BusController {
         String busStatus = pageInfo.getBusStatus();
         Integer currentPage = pageInfo.getCurrentPage();
         String key = RedisConstants.COUNTS_EXPIRE.getKey() + busStatus;
-        if (redisUtils.hasKey(key)) {
-            Object obj = redisUtils.lGetIndex(key, currentPage - 1);
-            if (null != obj) {
-                log.warn("getCount->redis:" + obj.toString());
-                return new ResponseUtil().setData(obj);
+        try {
+            if (redisUtils.hasKey(key)) {
+                Object obj = redisUtils.lGetIndex(key, currentPage - 1);
+                if (null != obj) {
+                    log.warn("getCount->redis\n");
+                    return new ResponseUtil().setData(obj);
+                }
             }
+            PageCountRequest request = new PageCountRequest();
+            request.setCurrentPage(pageInfo.getCurrentPage());
+            request.setPageSize(pageInfo.getPageSize());
+            request.setBusStatus(pageInfo.getBusStatus());
+            PageCountResponse response = busService.getCount(request);
+            // 写缓存
+            redisUtils.lSet(key, response, RedisConstants.COUNTS_EXPIRE.getTime());
+            log.warn("getCount\n");
+            return new ResponseUtil().setData(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("getCount\n");
+            CommonResponse response = new CommonResponse();
+            response.setCode(SbCode.SYSTEM_ERROR.getCode());
+            response.setMsg(SbCode.SYSTEM_ERROR.getMessage());
+            return new ResponseUtil().setData(response);
         }
-        PageCountRequest request = new PageCountRequest();
-        request.setCurrentPage(pageInfo.getCurrentPage());
-        request.setPageSize(pageInfo.getPageSize());
-        request.setBusStatus(pageInfo.getBusStatus());
-        PageCountResponse response = busService.getCount(request);
-        // 写缓存
-        redisUtils.lSet(key, response, RedisConstants.COUNTS_EXPIRE.getTime());
-        log.warn("getCount:" + response.toString());
-        return new ResponseUtil().setData(response);
+
     }
 
     @ApiOperation(value = "获取车次详情", notes = "获取车次详情", response = CountDetailResponse.class)
