15ff8d7cb6a0fc0167a85784b18c14fc3711978c
DreamCats <maifeng_cat@outlook.com>
2020-04-06 15:55:26 +0800

    创建雪花算法

--- school-bus/guns-api/src/main/java/com/stylefeng/guns/rest/myutils/UUIDUtils.java
+++ school-bus/guns-api/src/main/java/com/stylefeng/guns/rest/myutils/UUIDUtils.java
@@ -7,17 +7,20 @@
 
 package com.stylefeng.guns.rest.myutils;
 
+import cn.hutool.core.lang.Snowflake;
 import cn.hutool.core.util.IdUtil;
 
 import java.util.UUID;
 
 public class UUIDUtils {
 
+    private static Snowflake snowflake = IdUtil.createSnowflake(1, 1);
+
     public static String getUUID() {
         return IdUtil.randomUUID();
     }
 
-    public static String flakesUUID() {
-        return "";
+    public static Long flakesUUID() {
+        return snowflake.nextId();
     }
 }
